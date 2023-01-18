ba369055bec9816facbc8eb1bb072b2786cd77f8
gguilhermelopes <guilhermelopes275@gmail.com>
2023-01-18 15:41:51 -0300

    styled components e jest na prática

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import GlobalStyles from "@/styles/global";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link
+          rel="shortcut icon"
+          href="./img/icon-512.png"
+          type="image/x-icon"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="./img/icon-512.png"
+          type="image/x-icon"
+        />
+        <meta
+          name="description"
+          content="A simple project with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
